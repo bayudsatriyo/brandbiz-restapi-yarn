e3b66cdac93c79d391aae8ebb0db6e2ebe401992
bayudsatriyo <bayudsatriyo@gmail.com>
2024-07-20 18:17:46 +0700

    refactor: tidying userRoute

--- src/handler/userhandler.ts
+++ src/handler/userhandler.ts
@@ -1,49 +1,60 @@
-import userService from '../services/userService'
-import { type Response, type Request, type NextFunction } from 'express'
-import * as fs from 'fs'
+import userService from "../services/userService";
+import { type Response, type Request, type NextFunction } from "express";
+import * as fs from "fs";
 class Userhandler {
-  async registerUser (req: Request, res: Response, next: NextFunction): Promise<void> {
+  async registerUser(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<void> {
     try {
-      const users = req.body
-      console.log(users)
+      const users = req.body;
 
-      const result = await userService.addUsers(users)
+      const result = await userService.addUsers(users);
 
       res.status(201).json({
-        status: 'CREATED',
-        data: result
-      })
+        status: "CREATED",
+        data: result,
+      });
     } catch (e) {
-      next(e)
+      next(e);
     }
   }
 
-  async authenticationHandler (req: Request, res: Response, next: NextFunction): Promise<void> {
+  async authenticationHandler(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<void> {
     try {
-      const user = req.body
-      
-      const result = await userService.authentication(user)
+      const user = req.body;
+
+      const result = await userService.authentication(user);
       res.status(200).json({
-        status: 'SUCCESS',
-        data: result
-      })
+        status: "SUCCESS",
+        data: result,
+      });
     } catch (e) {
-      next(e)
+      next(e);
     }
   }
 
-  async getUserByUsernameHandler (req: Request, res: Response, next: NextFunction): Promise<void> {
+  async getUserByUsernameHandler(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<void> {
     try {
-      const username = req.user.username
+      const username = req.user.username;
 
-      const result = await userService.getUserByUsername(username)
+      const result = await userService.getUserByUsername(username);
 
       res.status(200).json({
-        status: 'SUCCESS',
-        data: result
-      })
+        status: "SUCCESS",
+        data: result,
+      });
     } catch (e) {
-      next(e)
+      next(e);
     }
   }
 
@@ -55,118 +66,134 @@ class Userhandler {
       // menggunakan metode fs.unlink untuk menghapus file
       fs.unlink(filePath, (err) => {
         if (err) {
-          console.log('gagal menghapus gambar')
+          return;
         }
       });
     }
-  }
+  };
 
-  async updateUserHandler (req: Request, res: Response, next: NextFunction): Promise<void> {
+  async updateUserHandler(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<void> {
     try {
-      const dataUser = req.body
-      const username = req.user.username
-      let image: string
-      console.log(req.file)
-      if(req.file !== undefined){
-        image = req.format
-        dataUser.profileUrl = `http://localhost:8080/brandbiz/user/image/${image}`
-      }else{
-        for (let ext of ['jpg', 'png', 'jpeg']) {
+      const dataUser = req.body;
+      const username = req.user.username;
+      let image: string;
+
+      if (req.file !== undefined) {
+        image = req.format;
+        dataUser.profileUrl = `http://localhost:8080/brandbiz/user/image/${image}`;
+      } else {
+        for (let ext of ["jpg", "png", "jpeg"]) {
           // membuat path lengkap dari file dengan nama dan ekstensi
           let filePath = `src/profile/${username}.${ext}`;
           // menggunakan metode fs.unlink untuk menghapus file
           fs.unlink(filePath, (err) => {
-            if(err){
-              console.log('sedang mencari')
+            if (err) {
+              console.log("sedang mencari");
             }
-            console.log('path/file.txt was deleted');
+            console.log("path/file.txt was deleted");
           });
         }
-        console.log('sudah dihapus')
-        dataUser.profileUrl = null
+
+        dataUser.profileUrl = null;
       }
 
-      console.log(dataUser)
-
-      const result = await userService.updateUser(dataUser, username)
+      const result = await userService.updateUser(dataUser, username);
 
       res.status(200).json({
-        status: 'UPDATED',
-        data: result
-      })
+        status: "UPDATED",
+        data: result,
+      });
     } catch (e) {
-      if(req.file !== undefined){
+      if (req.file !== undefined) {
         fs.unlinkSync(req.file.path);
       }
-      next(e)
+      next(e);
     }
   }
 
-  async logoutUserHandler (req: Request, res: Response, next: NextFunction): Promise<void> {
+  async logoutUserHandler(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<void> {
     try {
-      const username = req.user.username
+      const username = req.user.username;
 
-      const result = await userService.logoutUser(username)
+      const result = await userService.logoutUser(username);
 
       res.status(200).json({
-        status: 'LOGOUT',
-        data: `User dengan nama ${result} sudah logout`
-      })
+        status: "LOGOUT",
+        data: `User dengan nama ${result} sudah logout`,
+      });
     } catch (e) {
-      next(e)
+      next(e);
     }
   }
 
-  async usergetlearningHandler(req: Request, res: Response, next: NextFunction) {
+  async usergetlearningHandler(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) {
     try {
-        const useremail = req.user.email
-        const idLearning = parseInt(req.params.idLearning)
+      const useremail = req.user.email;
+      const idLearning = parseInt(req.params.idLearning);
 
-        const result = await userService.userGetLearning(useremail, idLearning)
+      const result = await userService.userGetLearning(useremail, idLearning);
 
-        res.status(200).json({
-            status: 'SUCCESS',
-            data: result
-        })
+      res.status(200).json({
+        status: "SUCCESS",
+        data: result,
+      });
     } catch (e) {
-        next(e)
+      next(e);
     }
   }
 
   async userUpdateSkor(req: Request, res: Response, next: NextFunction) {
     try {
-      const useremail = req.user.email
-      const idLearning = parseInt(req.params.idLearning)
-      const skorUser = parseInt(req.params.skor)
+      const useremail = req.user.email;
+      const idLearning = parseInt(req.params.idLearning);
+      const skorUser = parseInt(req.params.skor);
 
-      const result = await userService.updateSkorLearningPath(useremail, idLearning, skorUser)
+      const result = await userService.updateSkorLearningPath(
+        useremail,
+        idLearning,
+        skorUser
+      );
 
       res.status(200).json({
-        status: 'UPDATED',
-        data: result
-      })
+        status: "UPDATED",
+        data: result,
+      });
     } catch (e) {
-      next(e)
+      next(e);
     }
   }
 
-  async userAddFeedbackHandler(req: Request, res: Response, next: NextFunction) {
+  async userAddFeedbackHandler(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) {
     try {
-      const username = req.user.username
-      const note = req.body.note
+      const username = req.user.username;
+      const note = req.body.note;
 
-      console.log(username)
-      console.log(note)
-      const result = await userService.userAddFeedback(username, note)
+      const result = await userService.userAddFeedback(username, note);
 
       res.status(201).json({
-        status: 'SUCCESS',
-        data: result
-      })
+        status: "SUCCESS",
+        data: result,
+      });
     } catch (e) {
-      next(e)
+      next(e);
     }
   }
 }
 
-export default Userhandler
+export default Userhandler;
--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,139 +1,144 @@
-import bcrypt from 'bcrypt'
-import { v4 as uuid } from 'uuid'
-import prismaClient from '../applications/database'
-import ResponseError from '../exceptions/ResponseError'
-import validate from '../validations/validate'
-import usersValidation from '../validations/usersValidation'
-import { User } from '../../node_modules/.prisma/client/index'
-import learningValidation from '../validations/learningValidation'
+import bcrypt from "bcrypt";
+import { v4 as uuid } from "uuid";
+import prismaClient from "../applications/database";
+import ResponseError from "../exceptions/ResponseError";
+import validate from "../validations/validate";
+import usersValidation from "../validations/usersValidation";
+import { User } from "../../node_modules/.prisma/client/index";
+import learningValidation from "../validations/learningValidation";
 
 interface UserResponse {
-  email: string
-  username: string
-  fullname: string
+  email: string;
+  username: string;
+  fullname: string;
 }
 
 interface TokenUser {
-  token: string
+  token: string;
 }
 
 interface authenticationUser {
-  username: string, 
-  password: string
+  username: string;
+  password: string;
 }
 
 interface UserProfile {
-  email: string
-  username: string
-  fullname: string
-  alamat?: string | null
-  role?: string | null
-  profileUrl?: string | null
+  email: string;
+  username: string;
+  fullname: string;
+  alamat?: string | null;
+  role?: string | null;
+  profileUrl?: string | null;
 }
 
 const addUsers = async (users: User): Promise<UserResponse> => {
-  const userData: User = validate(usersValidation.addUsersValidation, users)
+  const userData: User = validate(usersValidation.addUsersValidation, users);
 
   const cekUser: number | null = await prismaClient.user.count({
     where: {
-      username: userData.username
-    }
-  })
+      username: userData.username,
+    },
+  });
 
   if (cekUser === 1) {
-    throw new ResponseError(400, 'Username atau email sudah digunakan')
+    throw new ResponseError(400, "Username atau email sudah digunakan");
   }
 
-  userData.password = await bcrypt.hash(userData.password, 10)
+  userData.password = await bcrypt.hash(userData.password, 10);
 
   const DataUser = await prismaClient.user.create({
     data: userData,
     select: {
       email: true,
       username: true,
-      fullname: true
-    }
-  })
+      fullname: true,
+    },
+  });
 
-  return DataUser as UserResponse
-}
+  return DataUser as UserResponse;
+};
 
-const authentication = async (users: authenticationUser): Promise<TokenUser> => {
-    const userData: User = validate(usersValidation.loginUserValidation, users)
+const authentication = async (
+  users: authenticationUser
+): Promise<TokenUser> => {
+  const userData: User = validate(usersValidation.loginUserValidation, users);
 
-    const cekUser: authenticationUser | null | undefined = await prismaClient.user.findUnique({
+  const cekUser: authenticationUser | null | undefined =
+    await prismaClient.user.findUnique({
       where: {
         username: userData.username,
       },
       select: {
         username: true,
-        password: true
-      }
-    })
-
-    if(cekUser === null || cekUser === undefined){
-      throw new ResponseError(400, 'Username atau password salah')
-    }
-    console.log(cekUser.password)
-    console.log(userData.password)
-    const cekPassword = await bcrypt.compare(userData.password, cekUser.password)
-    console.log(cekPassword);
-    if(cekPassword === false){
-      throw new ResponseError(404, 'Username atau Password salah')
-    }
-
-    const newToken = uuid().toString()
-    const UserToken = await prismaClient.user.update({
-      where: {
-        username: userData.username
+        password: true,
       },
-      data: {
-        token: newToken
-      },
-      select: {
-        token: true
-      }
-    })
+    });
 
-    return UserToken as TokenUser
-}
+  if (cekUser === null || cekUser === undefined) {
+    throw new ResponseError(400, "Username atau password salah");
+  }
+
+  const cekPassword = await bcrypt.compare(userData.password, cekUser.password);
+
+  if (cekPassword === false) {
+    throw new ResponseError(404, "Username atau Password salah");
+  }
+
+  const newToken = uuid().toString();
+  const UserToken = await prismaClient.user.update({
+    where: {
+      username: userData.username,
+    },
+    data: {
+      token: newToken,
+    },
+    select: {
+      token: true,
+    },
+  });
+
+  return UserToken as TokenUser;
+};
 
 const getUserByUsername = async (username: string): Promise<UserProfile> => {
-  validate(usersValidation.emailorusernameValidation, username)
+  validate(usersValidation.emailorusernameValidation, username);
 
   const cekUser = await prismaClient.user.count({
     where: {
-      username: username
-    }
-  })
+      username: username,
+    },
+  });
 
-  if(!cekUser){
-    throw new ResponseError(404, 'User tidak ditemukan')
+  if (!cekUser) {
+    throw new ResponseError(404, "User tidak ditemukan");
   }
 
   const dataUser = await prismaClient.user.findUnique({
     where: {
-      username: username
+      username: username,
     },
-    select:{
+    select: {
       username: true,
       email: true,
       fullname: true,
       alamat: true,
       role: true,
-      profileUrl: true
-    }
-  })
+      profileUrl: true,
+    },
+  });
 
-  return dataUser as UserProfile
-}
+  return dataUser as UserProfile;
+};
 
-const updateUser = async (userdata: UserProfile, username: string): Promise<UserProfile> => {
-  const dataUser = validate(usersValidation.updateValidation, userdata)
+const updateUser = async (
+  userdata: UserProfile,
+  username: string
+): Promise<UserProfile> => {
+  const dataUser = validate(usersValidation.updateValidation, userdata);
 
   const updateUser = await prismaClient.user.update({
     where: {
-      username: username
+      username: username,
     },
     data: dataUser,
     select: {
@@ -141,15 +146,15 @@ const updateUser = async (userdata: UserProfile, username: string): Promise<User
       email: true,
       fullname: true,
       role: true,
-      profileUrl: true
-    }
-  })
+      profileUrl: true,
+    },
+  });
 
-  return updateUser as UserProfile
-}
+  return updateUser as UserProfile;
+};
 
 const logoutUser = async (username: string): Promise<string> => {
-  validate(usersValidation.emailorusernameValidation, username)
+  validate(usersValidation.emailorusernameValidation, username);
 
   const deleteToken = await prismaClient.user.update({
     where: {
@@ -159,109 +164,117 @@ const logoutUser = async (username: string): Promise<string> => {
       token: null,
     },
     select: {
-      username: true
-    }
-  })
+      username: true,
+    },
+  });
 
-  return deleteToken.username as string
-}
+  return deleteToken.username as string;
+};
 
 const userGetLearning = async (useremail: string, idLearning: number) => {
-  const Useremail = validate(usersValidation.emailorusernameValidation, useremail)
-  const IdLearning = validate(learningValidation.idLearningpath, idLearning)
+  const Useremail = validate(
+    usersValidation.emailorusernameValidation,
+    useremail
+  );
+  const IdLearning = validate(learningValidation.idLearningpath, idLearning);
 
   const cekLearning = await prismaClient.learningpath.count({
     where: {
-      id: IdLearning
-    }
-  })
+      id: IdLearning,
+    },
+  });
 
-  if(!cekLearning){
-    throw new ResponseError(404, 'Learning Path tidak ditemukan')
+  if (!cekLearning) {
+    throw new ResponseError(404, "Learning Path tidak ditemukan");
   }
 
   const cekUserLearning = await prismaClient.userhaslearning.count({
     where: {
       useremail: Useremail,
-      learningId: IdLearning
-    }
-  })
+      learningId: IdLearning,
+    },
+  });
 
-  if(cekUserLearning >= 1){
-    throw new ResponseError(403, 'Learning path sudah diambil')
+  if (cekUserLearning >= 1) {
+    throw new ResponseError(403, "Learning path sudah diambil");
   }
 
   return prismaClient.userhaslearning.create({
     data: {
       useremail: Useremail,
-      learningId: IdLearning
+      learningId: IdLearning,
     },
     select: {
       useremail: true,
-      learningId: true
-    }
-  })
-}
+      learningId: true,
+    },
+  });
+};
 
-const updateSkorLearningPath = async (useremail: string, idLearning: number, skor: number) => {
-  const Useremail = validate(usersValidation.emailorusernameValidation, useremail)
-  const IdLearning = validate(learningValidation.idLearningpath, idLearning)
-  const skorUser = validate(usersValidation.skorValidation, skor)
+const updateSkorLearningPath = async (
+  useremail: string,
+  idLearning: number,
+  skor: number
+) => {
+  const Useremail = validate(
+    usersValidation.emailorusernameValidation,
+    useremail
+  );
+  const IdLearning = validate(learningValidation.idLearningpath, idLearning);
+  const skorUser = validate(usersValidation.skorValidation, skor);
 
   const cekLearning = await prismaClient.userhaslearning.findFirst({
     where: {
       useremail: Useremail,
-      learningId: IdLearning
-    }
-  })
+      learningId: IdLearning,
+    },
+  });
 
-  if(!cekLearning){
-    throw new ResponseError(404, 'Learning path tidak ada')
+  if (!cekLearning) {
+    throw new ResponseError(404, "Learning path tidak ada");
   }
 
   await prismaClient.userhaslearning.updateMany({
     where: {
       AND: [
         {
-          useremail: Useremail
+          useremail: Useremail,
         },
         {
-          learningId: IdLearning
-        }
-      ]
+          learningId: IdLearning,
+        },
+      ],
     },
     data: {
-      skor: skorUser
-    }
-  })
+      skor: skorUser,
+    },
+  });
 
-  return "Skor telah diupdate"
-}
+  return "Skor telah diupdate";
+};
 
 const userAddFeedback = async (username: string, note: string) => {
-  const noteFeedback = validate(usersValidation.feedbackValidation, note)
-  
+  const noteFeedback = validate(usersValidation.feedbackValidation, note);
+
   return prismaClient.feedback.create({
     data: {
       note: noteFeedback,
-      username: username
+      username: username,
     },
     select: {
       username: true,
-      note: true
-    }
-  })
-}
+      note: true,
+    },
+  });
+};
 
-
-
-export default { 
-      addUsers, 
-      authentication, 
-      getUserByUsername, 
-      updateUser, 
-      logoutUser, 
-      userGetLearning,
-      updateSkorLearningPath,
-      userAddFeedback
-}
+export default {
+  addUsers,
+  authentication,
+  getUserByUsername,
+  updateUser,
+  logoutUser,
+  userGetLearning,
+  updateSkorLearningPath,
+  userAddFeedback,
+};
